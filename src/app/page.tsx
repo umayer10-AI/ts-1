90e29cff517760daa0828e40b9331baf373d9bf9
umayer10-AI <mdumayer10@gmail.com>
2026-05-29 11:15:06 +0600

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 
 const page = () => {
   return (
     <div>
-      Hello Umayer
+      <h2 className='text-3xl font-bold my-20 text-center'>Hello Umayer</h2>
     </div>
   );
 };
